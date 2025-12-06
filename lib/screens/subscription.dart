d3d2f739853e5cae9dbf6d4ee450f106626a7c3d
NYANnPASU0 <162052515+NYANnPASU0@users.noreply.github.com>
2025-12-06 19:24:27 +0300

    update: create some text

--- lib/screens/subscription.dart
+++ lib/screens/subscription.dart
@@ -5,39 +5,60 @@ class Subscription extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(31, 132, 205, 1),
-                  Color.fromRGBO(219, 42, 107, 1),
-                ],
-                stops: [0.4, 0.6],
-                begin: Alignment(-6.0, -1.1),
-                end: Alignment(6.0, 0.8),
+    return MaterialApp(
+      home:Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(31, 132, 205, 1),
+                    Color.fromRGBO(219, 42, 107, 1),
+                  ],
+                  stops: [0.4, 0.6],
+                  begin: Alignment(-6.0, -1.1),
+                  end: Alignment(6.0, 0.8),
+                ),
               ),
             ),
-          ),
-          Positioned.fill(
-            child: Image.asset(
-              "images/rectangles.png",
-              fit: BoxFit.fill,
+            Positioned.fill(
+              child: Image.asset(
+                "images/rectangles.png",
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
-          Positioned(
-            top: 60,
-            left: 25,
-          child: SizedBox(
-            width: 14,
-            height: 13.3,
-            child: Image.asset("images/vect_back.png", fit: BoxFit.contain),
+            Positioned(
+              top: 60,
+              left: 25,
+              right: 25,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 14,
+                    height: 13.3,
+                    child: Image.asset(
+                      "images/vect_back.png",
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  SizedBox(width: 45,),
+                  Text(
+                    "Управление подпиской",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontFamily: 'Nunito',
+                      fontWeight: FontWeight.w800,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          
-        ],
+          ],
+        ),
       ),
     );
   }
